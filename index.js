c1afdf428bab55da0266cd5e8acaeb912b55c4ee
KarimSoto <karimtec2003@gmail.com>
2024-04-18 12:32:38 -0700

    index.js y home.html creados en la rama master

---
+++ index.js
@@ -0,0 +1,17 @@
+const equipos = {
+    america:{
+        nombre:"america",
+        apodo:"aguilas",
+        pais:"mexico",
+        titulos:14
+    },
+    chivas:{
+        nombre:"guadalajara",
+        apodo:"chivas",
+        pais:"mexico",
+        titulos:12
+    }
+}
+
+
+console.log(equipos);
